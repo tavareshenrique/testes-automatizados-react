af022f5e5eef12b399a47b2c79650ee99c01e71d
Henrique Tavares <ihenrits@gmail.com>
2019-07-06 17:48:13 -0300

    Testes E2E com TDD

---
+++ src/TodoList.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+// import { Container } from './styles';
+
+export default class TodoList extends Component {
+  state = {
+    newTodo: "",
+    todos: []
+  };
+
+  handleInputChange = e => {
+    this.setState({
+      newTodo: e.target.value
+    });
+  };
+
+  handleAddTodo = e => {
+    this.setState({
+      todos: [...this.state.todos, this.state.newTodo],
+      newTodo: ""
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.todos.map(todo => (
+            <li key={todo}>{todo}</li>
+          ))}
+        </ul>
+        <input
+          onChange={this.handleInputChange}
+          value={this.state.newTodo}
+          type="text"
+          name="todo"
+        />
+        <button onClick={this.handleAddTodo}>Adicionar</button>
+      </div>
+    );
+  }
+}
