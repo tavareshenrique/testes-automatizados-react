a458773313e3af15aa52b33e097f0518560fcf39
Henrique Tavares <ihenrits@gmail.com>
2019-07-07 14:32:41 -0300

    Mock da API com Axios

--- src/store/sagas/repositories.js
+++ src/store/sagas/repositories.js
@@ -1,14 +1,11 @@
 import { call, put } from "redux-saga/effects";
-import axios from "axios";
+import api from "../../services/api";
 
 import { Creators as RepositoriesActions } from "../ducks/repositories";
 
 export function* getRepositories() {
   try {
-    const response = yield call(
-      axios.get,
-      "https://api.github.com/users/tavareshenrique/repos"
-    );
+    const response = yield call(api.get, "/users/tavareshenrique/repos");
 
     yield put(RepositoriesActions.getSuccess(response.data));
   } catch (err) {
